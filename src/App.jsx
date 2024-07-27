fda11548eacd759f4e64c783cb96d78040ccfd45
Keith Price <keithprice.dev@gmail.com>
2024-07-27 20:23:00 +0100

    switched to fragments

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -67,6 +67,6 @@ export default function App() {
         </section>
         <h2>Time to get started!</h2>
       </main>
-    </div>
+    </>
   );
 }
